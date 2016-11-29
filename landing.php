1e497a3e6dcd3e71050754580013517d288578ff
Jason Kim <jason.kim1002578@gmail.com>
2016-11-29 15:39:49 -0500

    edited the copy on the landing page

--- landing.php
+++ landing.php
@@ -43,16 +43,16 @@
                       <h2 class="center orange-text"><i class="material-icons">favorite</i></h2>
                       <h5 class="center">Make new friends</h5>
 
-                      <p class="light">Connect with like-minded members of the RPI &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;community over a cup of coffee</p>
+                      <p class="light center-align">Connect with like-minded members of the RPI community over a cup of coffee</p>
                     </div>
                   </div>
 
                   <div class="col s12 m4">
                     <div class="icon-block">
                       <h2 class="center orange-text"><i class="material-icons">face</i></h2>
-                      <h5 class="center">Be a part of the community</h5>
+                      <h5 class="center">Engage your community</h5>
 
-                      <p class="light">Engage with the RPI community in a unique and &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;out-of-classroom experience</p>
+                      <p class="light center-align">Be a part of the RPI community in a unique and out-of-classroom experience</p>
                     </div>
                   </div>
 
@@ -61,7 +61,7 @@
                       <h2 class="center orange-text"><i class="material-icons">lightbulb_outline</i></h2>
                       <h5 class="center">Learn new things</h5>
 
-                      <p class="light">Share interesting thoughts and ideas to and inspire &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;others with your insight</p>
+                      <p class="light center-align">Share interesting thoughts and ideas to and inspire others with your insight</p>
                     </div>
                   </div>
                 </div>
