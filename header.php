3648fde6b77cbc69a98f9e9a6b97801dcf4cd65a
Jason Kim <jason.kim1002578@gmail.com>
2016-11-29 15:48:45 -0500

    alter the header

--- header.php
+++ header.php
@@ -6,19 +6,15 @@
           <div class="nav-wrapper">
             <a href="landing.php" class="brand-logo left"><img src="./images/cc-logo.png"></a>
             <ul class="right hide-on-med-and-down">
-              <!-- link to about page -->
               <li><a href="about.php">About</a></li>
-              <!-- link to meetups -->
               <li><a href="meetups.php">Meetups</a></li>
-              <!-- checks to see if the php -->
               <?php if(isset($_SESSION['fName'])){ ?>
-              <!-- link to logout time -->
               <li><a href="landing.php?logout">Logout</a></li>
+              <a class="waves-effect waves-light btn orange darken-4" href="createEvent.php">Host</a>
               <?php }else{ ?>
-              <!-- link ot login page -->
               <li><a href="login.php">Login</a></li>
-              <?php } ?>
               <a class="waves-effect waves-light btn orange darken-4" href="signup.php">Sign Up</a>
+              <?php } ?>
             </ul>
           </div>
         </div>
