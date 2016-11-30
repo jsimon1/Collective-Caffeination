9f06eb48ca577b0719d0ccefa00c855069ba945c
Jeremy Simon <jeremysimon2@gmail.com>
2016-11-30 14:44:47 -0500

    Bug fix for meetups.php, added footer.php

--- about.php
+++ about.php
@@ -111,36 +111,9 @@
       </div>
     </main>
 
-    <footer class="page-footer  brown darken-2">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <ul>
-                  <!-- link to this page -->
-                  <li><a class="grey-text text-lighten-3" href="about.html">About</a></li>
-                  <!-- link to the meetups page -->
-                  <li><a class="grey-text text-lighten-3" href="meetups.html">Meetups</a></li>
-                  <!-- links to the log in page -->
-                  <li><a class="grey-text text-lighten-3" href="login.html">Log In</a></li>
-                  <!-- link to the signup page -->
-                  <li><a class="grey-text text-lighten-3" href="signup.html">Sign Up</a></li>
-                  <!-- link to the gitup repo we created -->
-                  <li><a class="grey-text text-lighten-3" href="https://github.com/miknosaj/WebSys-Website">GitHub</a></li>
-                </ul>
-              </div>
-              <div class="col l6 s12">
-                <p class="grey-text text-lighten-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;At our first group project meeting, nearly all of us came with some form of caffeinated beverage in hand, so creating a web application that makes it easier for students to meet over a cup of coffee seemed like something that many RPI students would enjoy and could relate to.</br>
-                </br>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We knew we wanted to make something to help students at RPI who would not ordinarily meet, get a chance to go outside of their comfort zone and make friends on campus.</p>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright brown darken-4">
-            <div class="container">
-              Made with &#9749 by WebSys Group 1
-            </div>
-          </div>
-    </footer>
+    <?php
+      include("footer.php");
+    ?>
 
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
